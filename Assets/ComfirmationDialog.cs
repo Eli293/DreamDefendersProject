941eb9ae8415b55e54d32cb5a643418e365c18bd
Matthewtan9 <matthew999099@gmail.com>
2023-05-02 23:33:11 -0400

    -added few more functions
    -Tower can now shoot
    -Tower can be placed but (the movement is buggy)
    -added a second tower

--- Assets/ComfirmationDialog.cs
+++ Assets/ComfirmationDialog.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ConfirmationDialog : MonoBehaviour
 {
-    [SerializeField] public Turret turretToDelete;
+    [SerializeField] public List<Turret> turretToDelete;
     [SerializeField] private GameObject confirmationPanel;
     [SerializeField] private Button deleteButton;
     [SerializeField] private Button cancelButton;
@@ -14,10 +15,22 @@ public class ConfirmationDialog : MonoBehaviour
         cancelButton.onClick.AddListener(CancelDelete);
     }
 
+    public void AddTurretToDelete(Turret turret)
+    {
+        // Add turret to delete list
+        turretToDelete.Add(turret);
+    }
+
     public void ConfirmDelete()
     {
-        // Delete the turret
-        Destroy(turretToDelete.gameObject);
+        // Loop through list and delete each turret
+        foreach (Turret turret in turretToDelete)
+        {
+            Destroy(turret.gameObject);
+        }
+
+        // Clear the delete list
+        turretToDelete.Clear();
 
         // Hide the confirmation dialog
         confirmationPanel.SetActive(false);
@@ -25,6 +38,9 @@ public class ConfirmationDialog : MonoBehaviour
 
     public void CancelDelete()
     {
+        // Clear the delete list
+        turretToDelete.Clear();
+
         // Hide the confirmation dialog
         confirmationPanel.SetActive(false);
     }
