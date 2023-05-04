14ecd3e99cd20ffa5be9d63117f3e4d184cb86b6
Eli293 <119442915+Eli293@users.noreply.github.com>
2023-05-03 20:25:35 -0400

    IMP system

---
+++ Assets/IMPScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMPScript : MonoBehaviour
+{
+    public Text txtCurrency;
+    public int defaultCurrency;
+    public int currency;
+    public void Init()
+    {
+        currency = defaultCurrency;
+        UpdateUI();
+    }
+
+    public void Gain(int val)
+    {
+        currency = val; UpdateUI();
+    }
+
+    public bool Use(int val)
+    {
+        if (EnoughIMP(val))
+        {
+            currency -= val;
+            UpdateUI();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool EnoughIMP(int val)
+    {
+        //check if the val is enough
+        if(val >= currency)
+        {
+            return true;
+        }
+        else { return false; }
+    }
+
+    public void UpdateUI()
+    {
+        txtCurrency.text = currency.ToString();
+    }
+}
