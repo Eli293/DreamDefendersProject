2174e9f55db8632cde40bf87ffdf6509d8aed05b
Matthewtan9 <matthew999099@gmail.com>
2023-05-14 15:37:08 -0400

    -switch scene
    - the other scene woulndt not resume, so i fixed that and now it works fine.

--- Assets/Script/LVL2.cs
+++ Assets/Script/LVL2.cs
@@ -4,14 +4,17 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     public float delaySeconds = 3f; // Delay in seconds before switching scenes
+    private float timer = 0f; // Timer value to be stored and retrieved
 
     public void SwitchSceneWithDelay()
     {
+        timer = Time.time; // Store the current timer value
         Invoke("SwitchScene", delaySeconds);
     }
 
     private void SwitchScene()
     {
+        PlayerPrefs.SetFloat("Timer", timer); // Store the timer value in PlayerPrefs
         SceneManager.LoadScene("Lvl2");
     }
 }
--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 using System.Collections;
-using System.Collections.Generic;
-
 
 public class Timer : MonoBehaviour
 {
@@ -18,6 +17,7 @@ public class Timer : MonoBehaviour
         currentTime = countdownTime;
         winCanvas.enabled = false;
         loseCanvas.enabled = false;
+        Time.timeScale = 1f; // Set time scale to 1 in case it was paused in a previous scene
     }
 
     void Update()
@@ -28,18 +28,25 @@ public class Timer : MonoBehaviour
         {
             if (loseCanvas.enabled)
             {
-               
+                // Handle lose condition
             }
             else
             {
                 currentTime = 0;
                 winCanvas.enabled = true;
-                Time.timeScale = 0;
+                Time.timeScale = 0f; // Pause the game
+                StartCoroutine(SwitchSceneWithDelay());
             }
         }
 
         int minutes = Mathf.FloorToInt(currentTime / 60f);
-        int seconds = Mathf.FloorToInt(currentTime % 100f);
+        int seconds = Mathf.FloorToInt(currentTime % 60f);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    IEnumerator SwitchSceneWithDelay()
+    {
+        yield return new WaitForSeconds(3f); // Delay for 3 seconds
+        SceneManager.LoadScene("OtherScene"); // Replace "OtherScene" with the name of your next scene
     }
 }
